3937d732fc28e002f52385352dc31ea53e836962
Alex Rowley <rowleyaj@gmail.com>
2013-06-21 17:48:21 +0100

    Add controllers for Cars, Makes and Models

---
+++ application/modules/v1/controllers/CarsController.php
@@ -0,0 +1,18 @@
+<?php
+
+class V1_CarsController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
