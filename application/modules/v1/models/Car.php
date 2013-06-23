30bb75ed8c3cc48ec023e0632ad31104e50e3fd3
Alex Rowley <rowleyaj@gmail.com>
2013-06-23 19:58:16 +0100

    Add logic to car model

--- application/modules/v1/models/Car.php
+++ application/modules/v1/models/Car.php
@@ -2,6 +2,45 @@
 
 class V1_Model_Car
 {
+    private $id;
+    private $model_id;
+    private $production_year;
+    private $reg_number;
+    private $created_at;
+    private $modified_at;
 
+    public function __construct($row = null)
+    {
+        if (!is_null($row) and $row instanceof Zend_Db_Table_Row) {
+            $this->id = $row->id;
+            $this->model_id = $row->model_id;
+            $this->production_year = $row->production_year;
+            $this->reg_number = $row->reg_number;
+            $this->created_at = $row->created_at;
+            $this->modified_at = $row->modified_at;
+        }
+    }
 
+    public function __set($name, $value);
+    {
+        switch($name) {
+            case 'id':
+                if (!is_null($this->id)) {
+                    throw new Exception('Can not update id');
+                }
+                break;
+            case 'created_at':
+                if (!is_null($this->created_at)) {
+                    throw new Exception('Can not update created_at');
+                }
+                break;
+        }
+
+        $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
 }
