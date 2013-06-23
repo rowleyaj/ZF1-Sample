97517871ee943b656a49ef214e79694512a4e18f
Alex Rowley <rowleyaj@gmail.com>
2013-06-23 22:07:42 +0100

    Add model logic

--- application/modules/v1/models/Model.php
+++ application/modules/v1/models/Model.php
@@ -2,7 +2,43 @@
 
 class V1_Model_Model
 {
+    private $id;
+    private $make_id;
+    private $name;
+    private $created_at;
+    private $modified_at;
 
+    public function __construct($row = null)
+    {
+        if (!is_null($row) and $row instanceof Zend_Db_Table_Row) {
+            $this->id = $row->id;
+            $this->make_id = $row->make_id;
+            $this->name = $row->name;
+            $this->created_at = $row->created_at;
+            $this->modified_at = $row->modified_at;
+        }
+    }
 
+    public function __set($name, $value)
+    {
+        switch($name) {
+            case 'id':
+                if (!is_null($this->id)) {
+                    throw new Exception('Can not update id');
+                }
+                break;
+            case 'created_at':
+                if (!is_null($this->created_at)) {
+                    throw new Exception('Can not update created_at');
+                }
+                break;
+        }
+
+        $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
 }
-
