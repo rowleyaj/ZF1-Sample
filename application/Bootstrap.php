8c3fd223330a97de02c85c971c2f860fce41caff
Alex Rowley <rowleyaj@gmail.com>
2013-06-23 18:06:46 +0100

    Add rest routes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,15 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    public function _initRoutes()
+    {
+        $front = Zend_Controller_Front::getInstance();
+
+        $router = $front->getRouter();
+        $route = new Zend_Rest_Route($front, array(), array('v1'));
+
+        $router->addRoute('v1', $route);
+    }
 
 }
 
