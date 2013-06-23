a57d8858d822322ad35edf5b7d4ef6452e0b1029
Alex Rowley <rowleyaj@gmail.com>
2013-06-23 21:23:35 +0100

    Fix some silly key mistakes

--- application/data/schema.sql
+++ application/data/schema.sql
@@ -6,9 +6,8 @@ CREATE TABLE `cars` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  CONSTRAINT `cars_FK_model_id` FOREIGN KEY (`id`) REFERENCES `models` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `cars_FK_model_id` FOREIGN KEY (`model_id`) REFERENCES `models` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE `makes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -18,13 +17,12 @@ CREATE TABLE `makes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE `models` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `make_id` int(11) NOT NULL,
+  `make_id` int(11) unsigned NOT NULL,
   `name` varchar(20) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  CONSTRAINT `models_FK_make_id` FOREIGN KEY (`id`) REFERENCES `makes` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `models_FK_make_id` FOREIGN KEY (`make_id`) REFERENCES `makes` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
