6147d41db72ccc6d184006ab9fb4fc0186a1a831
Alex Rowley <rowleyaj@gmail.com>
2013-06-24 15:29:01 +0100

    Implement Makes

--- application/modules/v1/controllers/MakesController.php
+++ application/modules/v1/controllers/MakesController.php
@@ -2,22 +2,45 @@
 
 class V1_MakesController extends Zend_Rest_Controller
 {
+    protected $mapper;
+    protected $id;
 
     public function init()
     {
         $this->_helper->viewRenderer->setNoRender(true);
+        $this->mapper = new V1_Model_Mapper_Make;
+        $this->id = $this->_getParam('id');
     }
 
     public function indexAction()
     {
         // v1/makes/
         // Display all makes
+        $makes = $this->mapper->getAllMakes();
+
+        $json = '{ "response":[';
+        foreach ($makes as $make) {
+            $json .= Zend_Json::encode($make);
+            $json .= ',';
+        }
+        $json = trim($json, ',');
+        $json .= ']}';
+
+        $this->getResponse()->setBody($json);
+        $this->getResponse()->setHttpResponseCode(200);
+
     }
 
     public function getAction()
     {
         // v1/makes/:id
         // Display one make
+        $make = $this->mapper->getMakeById($this->id);
+
+        $json = Zend_Json::encode($make);
+
+        $this->getResponse()->setBody($json);
+        $this->getResponse()->setHttpResponseCode(200);
     }
 
     public function postAction()
@@ -25,6 +48,17 @@ class V1_MakesController extends Zend_Rest_Controller
         // v1/makes
         // with data
         // Create a make
+        $rawBody = $this->getRequest()->getRawBody();
+        $data = Zend_Json::decode($rawBody);
+
+        $make = new V1_Model_Make;
+        $make->loadFromArray($data);
+        $make = $this->mapper->save($make);
+
+        $json = Zend_Json::encode($make);
+        $this->getResponse()->setBody($json);
+        $this->getResponse()->setHttpResponseCode(201);
+
     }
 
     public function putAction()
@@ -32,17 +66,31 @@ class V1_MakesController extends Zend_Rest_Controller
         // v1/makes/:id
         // with data
         // Update a make
+        $rawBody = $this->getRequest()->getRawBody();
+        $data = Zend_Json::decode($rawBody);
+
+        $make = $this->mapper->getMakeById($this->id);
+
+        $make->loadFromArray($data);
+        $make = $this->mapper->save($make);
+
+        $json = Zend_Json::encode($make);
+        $this->getResponse()->setBody($json);
+        $this->getResponse()->setHttpResponseCode(200);
     }
 
     public function deleteAction()
     {
         // v1/makes/:id
         // Delete a make
+        $make = $this->mapper->deleteById($this->id);
+        $this->getResponse()->setHttpResponseCode(204);
     }
 
     public function headAction()
     {
         // Return only headers for a request
+        // // v1/makes/:id
+        // Display one make
     }
 }
-
--- application/modules/v1/models/mappers/Make.php
+++ application/modules/v1/models/mappers/Make.php
@@ -9,11 +9,13 @@ class V1_Model_Mapper_Make extends V1_Model_Mapper_Abstract
 
     public function save(V1_Model_Make $make)
     {
+        $data = array();
+
         $data = array(
-            'id' => $make->id,
-            'name' => $make->model_id,
-            'created_at' => $make->created_at,
-            'modified_at' => $make->modified_at
+            'id' => (int) $make->_id,
+            'name' => $make->_name,
+            'created_at' => $make->_created_at,
+            'modified_at' => $make->_modified_at
         );
 
         // Remove created_at to prevent modification
@@ -21,14 +23,26 @@ class V1_Model_Mapper_Make extends V1_Model_Mapper_Abstract
             unset($data['created_at']);
         }
 
-        if (is_null($make->id)) {
-            $data['created_at'] = date('Y-m-d H:i:s');
-            $data['modified_at'] = date('Y-m-d H:i:s');
+        if (is_null($make->_id)) {
+            date_default_timezone_set('UTC');
+            $now = date('Y-m-d H:i:s');
+            $data['created_at'] = $now;
+            $data['modified_at'] = $now;
             $this->_db_table->insert($data);
+            $id = $this->_db_table->getAdapter()->lastInsertId();
+            $make->_id = $id;
+            $make->_created_at = $now;
+            $make->_modified_at = $now;
+
         } else {
-            $data['modified_at'] = date('Y-m-d H:i:s');
-            $this->_db_table->update($data, array('id =?' => $make->id));
+            date_default_timezone_set('UTC');
+            $now = date('Y-m-d H:i:s');
+            $data['modified_at'] = $now;
+            $this->_db_table->update($data, array('id =?' => $make->_id));
+            $make->_modified_at = $now;
         }
+
+        return $make;
     }
 
     public function getMakeById($id)
@@ -47,6 +61,8 @@ class V1_Model_Mapper_Make extends V1_Model_Mapper_Abstract
         foreach ($rowset as $row) {
             $makes[] = new V1_Model_Make($row);
         }
-    }
 
+        return $makes;
+    }
 }
+
