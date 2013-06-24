416603691259ee8276d72157d313ce8f438cd497
Alex Rowley <rowleyaj@gmail.com>
2013-06-24 15:11:33 +0100

    Add loadFromArray and toJson functions

--- application/modules/v1/models/Abstract.php
+++ application/modules/v1/models/Abstract.php
@@ -11,12 +11,12 @@ abstract class V1_Model_Abstract
     public function __set($name, $value)
     {
         switch($name) {
-            case 'id':
+            case '_id':
                 if (!is_null($this->_id)) {
                     throw new Exception('Can not update id');
                 }
                 break;
-            case 'created_at':
+            case '_created_at':
                 if (!is_null($this->_created_at)) {
                     throw new Exception('Can not update created_at');
                 }
@@ -29,5 +29,37 @@ abstract class V1_Model_Abstract
     public function __get($name)
     {
         return $this->$name;
+    }
+
+    public function loadFromArray(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if ($key !== 'id' and $key !== 'created_at' and $key !== 'modified_at'){
+                $key = '_' . $key;
+                $this->$key = $value;
+            }
+        }
+    }
+
+    public function toJson()
+    {
+        $array = (array) $this;
+        $class = get_called_class();
+        $regex = '/(\W|^)(' . $class . '|\*|_)/';
+
+        foreach ($array as $key => $value) {
+            unset($array[$key]);
+            $key = preg_replace($regex, '', $key);
+            $array[$key] = $value;
+        }
+
+        $json = '{';
+        foreach ($array as $key => $value) {
+            $json .= "\"$key\": \"$value\",";
+        }
+        $json = trim($json, ',');
+        $json .= '}';
+
+        return $json;
     }
 }
